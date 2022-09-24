22e953e3a25b1f5a8bab4b34f925148129ab27bb
Nicholas Bailey <nickcyg1992@gmail.com>
2022-09-23 19:15:26 -0500

    flesh out routes and controller funcs

--- controllers/pedalboards.js
+++ controllers/pedalboards.js
@@ -1,1 +1,75 @@
 import { Pedalboard } from "../models/pedalboard";
+
+function create(req, res) {
+  req.body.owner = req.user.profile;
+  Pedalboard.create(req.body)
+    .then((pedalboard) => {
+      Pedalboard.findById(pedalboard._id)
+        .populate("owner")
+        .then((populatedPedalboard) => {
+          res.json(populatedPedalboard);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.errmsg });
+    });
+}
+
+function index(req, res) {
+  Pedalboard.find({})
+    .populate("owner")
+    .then((pedalboards) => {
+      res.json(pedalboards);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.errmsg });
+    });
+}
+
+function update(req, res) {
+  Pedalboard.findById(req.params.id)
+    .then((pedalboard) => {
+      if (pedalboard.owner._id.equals(req.user.profile)) {
+        Pedalboard.findByIdAndUpdate(req.params.id, req.body, { new: true })
+          .then((updatedPedalboard) => {
+            res.json(updatedPedalboard);
+          });
+      } else {
+        res.status(401).json({ msg: "Not Authorized" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.errmsg });
+    });
+}
+
+function deleteOne(req, res) {
+  Pedalboard.findById(req.params.id)
+    .then((pedalboard) => {
+      if (pedalboard.owner._id.equals(req.user.profile)) {
+        Pedalboard.findByIdAndDelete(pedalboard._id)
+          .then((deletedPedalboard) => {
+            res.json(deletedPedalboard);
+          });
+      } else {
+        res.status(401).json({ msg: "Not Authorized" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.errmsg });
+    });
+}
+
+export {
+  create,
+  index,
+  update,
+  deleteOne,
+};
+
+
+
--- routes/pedalboards.js
+++ routes/pedalboards.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
-import * as pedalsCtrl from '../controllers/pedals.js'
+import * as pedalboardCtrl from '../controllers/pedals.js'
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
+
+/*---------- Public Routes ----------*/
+router.get('/', pedalboardCtrl.index)
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, pedalboardCtrl.create)
+router.delete('/:id', checkAuth, pedalboardCtrl.delete)
+router.put('/:id', checkAuth, pedalboardCtrl.update)
 
 
 
